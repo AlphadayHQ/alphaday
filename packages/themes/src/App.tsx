08179e28b42192628aa29036ac353e0db9b88472
Xharles <charlesnzubechukwu000@gmail.com>
2023-07-13 08:50:33 +0100

    add ionic

--- packages/themes/src/App.tsx
+++ packages/themes/src/App.tsx
@@ -1,9 +1,17 @@
-function App() {
+import { IonDatetime, setupIonicReact } from "@ionic/react";
+import "@ionic/react/css/core.css";
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
 
+setupIonicReact();
+
+function App() {
   return (
-    <>
-      <h1 className="text-lg text-red-700">Vite + React</h1>
-    </>
+    <div className="w-screen h-screen flex flex-col justify-center items-center">
+      <h1 className="text-lg text-red-700">Vite + React + Ionic</h1>
+      <IonDatetime className="mt-10"></IonDatetime>
+    </div>
   );
 }
 
