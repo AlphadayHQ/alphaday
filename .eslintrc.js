c73b1e45248cdedc15e81068039d009e3f838052
Xharles <charlesnzubechukwu000@gmail.com>
2023-07-13 08:28:35 +0100

    fix parserOptions for tailwind config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,15 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
-    'plugin:react/recommended',
-    'eslint:recommended'
-  ],
+  extends: ["plugin:react/recommended", "eslint:recommended"],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
+    sourceType: "module",
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-  }
-}
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  },
+};
