5af542925d405797e8453b3915eb5a57cfe9e701
Xharles <charlesnzubechukwu000@gmail.com>
2023-07-13 08:35:15 +0100

    set up theme

---
+++ packages/theme/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-lg text-red-700">Vite + React</h1>
+    </>
+  );
+}
+
+export default App;
