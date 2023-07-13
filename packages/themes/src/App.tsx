2cac2f668616a5d443a305e0c4a3a470f1877776
Xharles <charlesnzubechukwu000@gmail.com>
2023-07-13 09:38:23 +0100

    add scss

--- packages/themes/src/App.tsx
+++ packages/themes/src/App.tsx
@@ -1,15 +1,11 @@
 import { IonDatetime, setupIonicReact } from "@ionic/react";
-import "@ionic/react/css/core.css";
-import "@ionic/react/css/normalize.css";
-import "@ionic/react/css/structure.css";
-import "@ionic/react/css/typography.css";
 
 setupIonicReact();
 
 function App() {
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center">
-      <h1 className="text-lg text-red-700">Vite + React + Ionic</h1>
+      <h1 className="text-lg text-red-700 font-semibold">Vite + React + Ionic</h1>
       <IonDatetime className="mt-10"></IonDatetime>
     </div>
   );
