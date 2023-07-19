cf8126a119a72ca799355507d9b90e32e3c8b0d3
Xharles <charlesnzubechukwu000@gmail.com>
2023-07-19 10:27:21 +0100

    add alphaCheckbox

---
+++ packages/ui-kit/src/components/checkboxes/AlphaCheckbox.stories.tsx
@@ -0,0 +1,9 @@
+import { AlphaCheckbox } from "./AlphaCheckbox";
+
+export default {
+    title: "Widgets/AlphaCheckbox",
+    component: AlphaCheckbox,
+    argTypes: {},
+};
+
+export { AlphaCheckbox };
