274647255aab08c20ec7b8a1ceca7e250aa31c8d
Xharles <charlesnzubechukwu000@gmail.com>
2023-07-24 19:21:58 +0100

    rebase changes

--- packages/ui-kit/src/components/checkboxes/AlphaCheckbox.stories.tsx
+++
@@ -1,9 +0,0 @@
-import { AlphaCheckbox } from "./AlphaCheckbox";
-
-export default {
-    title: "Widgets/AlphaCheckbox",
-    component: AlphaCheckbox,
-    argTypes: {},
-};
-
-export { AlphaCheckbox };
