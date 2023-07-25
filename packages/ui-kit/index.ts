11e037b303ec3d3d6be512ffefb445bc355d5b35
Xharles <charlesnzubechukwu000@gmail.com>
2023-07-25 18:38:03 +0100

    test

--- packages/ui-kit/index.ts
+++ packages/ui-kit/index.ts
@@ -1,13 +1,13 @@
+import { NavTabButton } from "./src/components/buttons/NavTabButton";
 import Footer from "./src/components/layout/Footer";
-import Logo from "./src/components/layout/Logo";
 import {
     HeaderWrapper,
     HeaderNavbar,
     HeaderNavRight,
     HeaderNavElement,
 } from "./src/components/layout/Header";
+import Logo from "./src/components/layout/Logo";
 
-import { NavTabButton } from "./src/components/buttons/NavTabButton";
 import { breakpoints } from "./src/globalStyles/breakpoints";
 
 export {
