87c91aba4a63c32292d5167b9b0019b1912332ff
Xharles <charlesnzubechukwu000@gmail.com>
2023-07-25 18:33:57 +0100

    move Footer to components

--- packages/frontend/src/layouts/Footer.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Footer: React.FC = () => (
-    <footer className="fontGroup-mini border-t-btnRingVariant300 bg-background w-full border-t border-solid px-[25px] py-[15px] uppercase" />
-);
-
-export default Footer;
