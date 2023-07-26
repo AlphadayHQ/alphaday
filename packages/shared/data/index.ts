13d50be780d299d01ebfeccd97e52c0504f5069d
Xharles <charlesnzubechukwu000@gmail.com>
2023-07-26 20:00:26 +0100

    remove data files

--- packages/shared/data/index.ts
+++
@@ -1,5 +0,0 @@
-import menuData from "./menu";
-import asideMenuData from "./menu-aside";
-import { MapKey, MapBoxAccessKey } from "./api-keys";
-
-export { menuData, asideMenuData, MapKey, MapBoxAccessKey };
