104371e2eeac86914f5c302f11c5f746c91b5266
Xharles <charlesnzubechukwu000@gmail.com>
2023-07-27 10:11:51 +0100

    add brakpoints

--- packages/ui-kit/tailwind.config.js
+++ packages/ui-kit/tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 import { alphadayColors } from "./src/globalStyles/colors";
+import { deviceBreakpoints } from "./src/globalStyles/breakpoints";
 import typography from "@tailwindcss/typography";
+
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
         extend: {},
         colors: alphadayColors,
+        screens: {
+            ...deviceBreakpoints,
+            tiny: { max: deviceBreakpoints.tiny }, // tailwind defaults to min-width. This means @media (max-width: tiny) { ... }
+        },
     },
     plugins: [typography],
 };
