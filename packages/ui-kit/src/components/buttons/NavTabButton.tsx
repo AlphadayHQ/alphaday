5fbc5678f6a344c4e3bf03feb3b845b4733316ac
Charles Nwankwo <39612820+Xavier-Charles@users.noreply.github.com>
2023-07-31 22:24:06 +0100

    feat: ALPHA-3693 Add Layout specific components to ui-kit (#12)

---
+++ packages/ui-kit/src/components/buttons/NavTabButton.tsx
@@ -0,0 +1,57 @@
+import { FC } from "react";
+import CloseSVG from "src/assets/alphadayAssets/icons/close.svg";
+import ViewsSVG from "src/assets/alphadayAssets/icons/views.svg";
+import WidgetsSVG from "src/assets/alphadayAssets/icons/widgets.svg";
+import { TabButton } from "./TabButton";
+
+interface ButtonProps {
+    variant: "views" | "modules";
+    open: boolean;
+    disabled?: boolean;
+    uppercase?: boolean;
+    label?: string;
+    title?: string;
+    onClick?: () => MaybeAsync<void>;
+    children?: React.ReactNode;
+}
+
+export const NavTabButton: FC<ButtonProps> = ({
+    children,
+    variant,
+    open,
+    disabled,
+    uppercase,
+    label,
+    ...restProps
+}) => {
+    return (
+        <TabButton
+            open={open}
+            disabled={disabled}
+            uppercase={uppercase}
+            aria-label={label}
+            {...restProps}
+        >
+            {variant === "modules" &&
+                (open ? (
+                    <CloseSVG className="tabButton" />
+                ) : (
+                    <WidgetsSVG className="tabButton widgets" />
+                ))}
+            {variant === "views" &&
+                (open ? (
+                    <CloseSVG className="tabButton" />
+                ) : (
+                    <ViewsSVG className="tabButton" />
+                ))}
+            {children}
+        </TabButton>
+    );
+};
+
+NavTabButton.defaultProps = {
+    open: false,
+    disabled: false,
+    uppercase: true,
+    label: "button",
+};
