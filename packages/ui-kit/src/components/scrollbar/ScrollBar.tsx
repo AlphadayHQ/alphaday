71ead323ab20777a615bc41fb3ede57a99251ff2
Xharles <charlesnzubechukwu000@gmail.com>
2023-07-31 19:25:28 +0100

    add scrollbar

---
+++ packages/ui-kit/src/components/scrollbar/ScrollBar.tsx
@@ -0,0 +1,28 @@
+import { FC } from "react";
+import PerfectScrollbar, { ScrollBarProps } from "react-perfect-scrollbar";
+import "react-perfect-scrollbar/dist/css/styles.css";
+import styles from "./scrollbar.module.scss"
+import { twMerge } from "tailwind-merge";
+import { Z_INDEX_REGISTRY } from "src/config/zIndexRegistry";
+
+export const ScrollBar: FC<ScrollBarProps> = ({
+    children,
+    onScroll,
+    containerRef,
+    onYReachEnd,
+    className,
+    ...rest
+}) => {
+    return (
+        <div className={twMerge(`[&>.ps>.ps__rail-y]:z-[${Z_INDEX_REGISTRY.SCROLLBAR}]`, styles.scrollbar, className)}  {...rest}>
+            <PerfectScrollbar
+                containerRef={containerRef}
+                onYReachEnd={onYReachEnd}
+                onScroll={onScroll}
+                {...rest}
+            >
+                {children}
+            </PerfectScrollbar>
+        </div>
+    );
+};
