d59b43625c165586a7a226658541308b46439b2d
Xharles <charlesnzubechukwu000@gmail.com>
2023-08-08 17:29:20 +0100

    clean up

--- packages/ui-kit/src/components/search/Searchbar.tsx
+++ packages/ui-kit/src/components/search/Searchbar.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect, useCallback } from "react";
-import { themes } from "@alphaday/ui-kit";
 import Select, {
     components,
     GroupBase,
@@ -15,6 +14,7 @@ import Select, {
 } from "react-select";
 // TODO (xavier-charles): add slugify util
 // import { slugify } from "src/api/utils/textUtils";
+import { themes } from "src/globalStyles";
 import { ReactComponent as HotSVG } from "../../assets/svg/hot.svg";
 import { Spinner } from "../spinner/Spinner";
 
