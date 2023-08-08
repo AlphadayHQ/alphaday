ba9565f46fd16b7589abee4cf56b29c44e90b4da
Xharles <charlesnzubechukwu000@gmail.com>
2023-08-08 16:22:36 +0100

    move sync indicator

--- packages/ui-kit/index.ts
+++ packages/ui-kit/index.ts
@@ -9,6 +9,9 @@ import { Button } from "src/components/buttons/Button";
 import { NavTabButton } from "src/components/buttons/NavTabButton";
 import { TabButton } from "src/components/buttons/TabButton";
 import { Dialog } from "src/components/dialog/Dialog";
+import SyncIndicator, {
+    EIndicatorState,
+} from "src/components/header/syncIndicator/SyncIndicator";
 import Footer from "src/components/layout/Footer";
 import {
     HeaderWrapper,
@@ -21,9 +24,6 @@ import Logo from "src/components/layout/Logo";
 import { Modal } from "src/components/modal/Modal";
 import { ScrollBar } from "src/components/scrollbar/ScrollBar";
 import { SearchBar } from "src/components/search/Searchbar";
-import SyncIndicator, {
-    EIndicatorState,
-} from "src/components/syncIndicator/SyncIndicator";
 import { breakpoints } from "src/globalStyles/breakpoints";
 import { twMerge } from "tailwind-merge";
 import { themes } from "src/globalStyles";
