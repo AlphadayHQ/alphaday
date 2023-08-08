a5fd945ac3bf83bcd6ebd3572a770b2c9b954b89
Xharles <charlesnzubechukwu000@gmail.com>
2023-08-08 14:34:17 +0100

    add ui files to frontend content

--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -2,5 +2,5 @@ import tailwindConfig from "@alphaday/ui-kit/tailwind.config";
 
 export default {
     ...tailwindConfig,
-    content: ["./index.html", "./src/**/*.{ts,tsx}"],
+    content: ["./index.html", "./src/**/*.{ts,tsx}", "../ui-kit/src/**/*.{ts,tsx}"],
 };
