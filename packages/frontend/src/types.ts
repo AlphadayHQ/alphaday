61dc89545d796ca62c779017a47a8062a2041fb2
Xharles <charlesnzubechukwu000@gmail.com>
2023-09-13 15:11:42 +0100

    add items widget

--- packages/frontend/src/types.ts
+++ packages/frontend/src/types.ts
@@ -57,6 +57,7 @@ export type TTemplatesDict = {
 
 export const TEMPLATES_DICT: Partial<TTemplatesDict> = {
     news_template: lazy(() => import("./containers/items/ItemsContainer")),
+    dao_template: lazy(() => import("./containers/items/ItemsContainer")),
     sample_template: lazy(() => import("./containers/sample/SampleContainer")),
 };
 
