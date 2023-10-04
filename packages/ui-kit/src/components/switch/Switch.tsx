6ee5733b89f19adf6b6b6a0d9015db822658b100
Xharles <charlesnzubechukwu000@gmail.com>
2023-10-04 17:19:48 +0100

    style the switch properly

--- packages/ui-kit/src/components/switch/Switch.tsx
+++ packages/ui-kit/src/components/switch/Switch.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import { twMerge } from "tailwind-merge";
 import styles from "./Switch.module.scss";
 
 export interface SwitchProps {
@@ -17,15 +18,18 @@ export const Switch: FC<SwitchProps> = ({
     disabled,
     label,
     className,
+    onChange,
     ...restProps
 }) => {
     return (
         <label
             htmlFor="switch"
             aria-label={label}
-            className={`${styles.switch} ${
-                disabled ? styles.disabled : ""
-            } ${className}`}
+            className={twMerge(
+                "relative inline-block text-primary",
+                disabled ? styles.disabled : "",
+                className
+            )}
             {...restProps}
         >
             <input
@@ -33,10 +37,26 @@ export const Switch: FC<SwitchProps> = ({
                 type="checkbox"
                 aria-label={label}
                 checked={checked}
+                className="opacity-0 z-[1] w-full h-full absolute cursor-pointer"
+                onChange={onChange}
             />
-            <span className="slider">
-                <span className="options option1">{options[0]}</span>
-                <span className="options option2">{options[1]}</span>
+            <span className="flex justify-between items-center cursor-pointer bg-btnBackgroundVariant1100 rounded-[10px] transition duration-[ms] ease-in-out">
+                <span
+                    className={twMerge(
+                        "m-[3px] text-center capitalize fontGroup-highlightSemi duration-[400ms] rounded-lg py-[3px] px-2 min-w-min two-col:min-w-[62px] hover:bg-btnBackgroundVariant1200 active:bg-btnBackgroundVariant1600 option1",
+                        !checked && "bg-btnBackgroundVariant1400"
+                    )}
+                >
+                    {options[0]}
+                </span>
+                <span
+                    className={twMerge(
+                        "m-[3px] text-center capitalize fontGroup-highlightSemi duration-[400ms] rounded-lg py-[3px] px-2 min-w-min two-col:min-w-[62px] hover:bg-btnBackgroundVariant1200 active:bg-btnBackgroundVariant1600 option2",
+                        checked && "bg-btnBackgroundVariant1400"
+                    )}
+                >
+                    {options[1]}
+                </span>
             </span>
         </label>
     );
