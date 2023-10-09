3420c3650f53bb700b1dbd3faed1bdbf06d3e13c
elcharitas <jonathanirhodia@gmail.com>
2023-10-09 02:42:30 +0100

    regsieter summary widget

--- packages/frontend/src/types.ts
+++ packages/frontend/src/types.ts
@@ -63,6 +63,9 @@ export const TEMPLATES_DICT: Partial<TTemplatesDict> = {
     news_template: lazy(() => import("./containers/items/ItemsContainer")),
     reddit_template: lazy(() => import("./containers/items/ItemsContainer")),
     sample_template: lazy(() => import("./containers/sample/SampleContainer")),
+    summary_template: lazy(
+        () => import("./containers/summary/SummaryContainer")
+    ),
 };
 
 export type TFullSizeRoute = {
