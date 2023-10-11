7b3e1a6a017eeef07875ec562cce1114d2f5b9ed
elcharitas <jonathanirhodia@gmail.com>
2023-10-11 15:59:53 +0100

    fix next page

--- packages/frontend/src/containers/lens-feed/LensFeedContainer.tsx
+++ packages/frontend/src/containers/lens-feed/LensFeedContainer.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, FC, useRef, useCallback } from "react";
-import { useWidgetHeight } from "src/api/hooks";
+import { usePagination, useWidgetHeight } from "src/api/hooks";
 import { TBaseTag, useGetLensItemsQuery } from "src/api/services";
 import { TLensPost } from "src/api/types";
 import { filteringListToStr } from "src/api/utils/filterUtils";
@@ -7,19 +7,9 @@ import {
     buildUniqueItemList,
     itemListsAreEqual,
 } from "src/api/utils/itemUtils";
-import { extractPaginationNumbers } from "src/api/utils/pagination";
 import LensFeedModule from "src/components/feeds/LensFeedModule";
 import { EWidgetSettingsRegistry } from "src/constants";
 import { IModuleContainer } from "src/types";
-
-// create function to debounce the paginate function
-const debounce = (func: () => void, wait: number) => {
-    let timeout: ReturnType<typeof setTimeout>;
-    return () => {
-        clearTimeout(timeout);
-        timeout = setTimeout(func, wait);
-    };
-};
 
 const LensFeedContainer: FC<IModuleContainer> = ({ moduleData }) => {
     const [posts, setItems] = useState<TLensPost[] | undefined>();
@@ -36,10 +26,34 @@ const LensFeedContainer: FC<IModuleContainer> = ({ moduleData }) => {
     const tagsRef = useRef<TBaseTag[]>();
     const tags = tagsSettings.find(String)?.tags; // tagsSettings[0] !== undefined ? tagsSettings[0].tags : undefined
 
-    const { data, isLoading } = useGetLensItemsQuery({
+    const { data, isLoading, isSuccess } = useGetLensItemsQuery({
         page: currentPage,
         tags: tags ? filteringListToStr(tags) : undefined,
     });
+
+    const { nextPage, handleNextPage } = usePagination(
+        data?.links,
+        10,
+        isSuccess
+    );
+
+    const handlePaginate = useCallback(() => {
+        handleNextPage("next");
+    }, [handleNextPage]);
+
+    // set current page 350ms after next page is set.
+    // RTK should cache requests, so we don't need to be too careful about rerenders.
+    useEffect(() => {
+        if (nextPage === undefined) {
+            return () => null;
+        }
+        const timeout = setTimeout(() => {
+            setCurrentPage(nextPage);
+        }, 350);
+        return () => {
+            clearTimeout(timeout);
+        };
+    }, [nextPage]);
 
     // reset results when tags preferences changed
     useEffect(() => {
@@ -75,20 +89,11 @@ const LensFeedContainer: FC<IModuleContainer> = ({ moduleData }) => {
         }
     }, [data?.results]);
 
-    const pagination = extractPaginationNumbers(data?.links);
-
-    /**
-     * Create a debounced version of the paginate function to avoid calling it
-     * multiple times when the user scrolls. Only call it when the user has
-     * stopped scrolling for 500ms. Ensure repeated calls to the debounced
-     * function will not reset the timer.
-     */
-
     return (
         <LensFeedModule
             posts={posts || []}
             isLoading={isLoading}
-            handlePaginate={() => {}}
+            handlePaginate={handlePaginate}
             widgetHeight={widgetHeight}
         />
     );
