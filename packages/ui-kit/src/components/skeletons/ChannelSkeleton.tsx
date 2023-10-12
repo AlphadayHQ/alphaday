1ddefad9572bfdb1e3e1e9f7781c3745111f53b6
Xharles <charlesnzubechukwu000@gmail.com>
2023-10-12 14:30:01 +0100

    Add skeleton

---
+++ packages/ui-kit/src/components/skeletons/ChannelSkeleton.tsx
@@ -0,0 +1,26 @@
+import { FC } from "react";
+import Skeleton from "react-loading-skeleton";
+
+const ChannelSkeleton: FC = () => {
+    return (
+        <div className="p-[5px] pl-[10px] flex flex-col bg-backgroundVariant200 hover:bg-backgroundVariant200">
+            <div className="w-[50px] h-auto [&>a]:block [&:img]:w-[50px] [&:img]:h-[50px] [&:img]:rounded-[4px] [&:img]:object-cover">
+                <Skeleton
+                    style={{
+                        width: "60px",
+                        height: "60px",
+                        borderRadius: "4px",
+                    }}
+                />
+            </div>
+            <Skeleton
+                style={{
+                    width: "60px",
+                    marginTop: "14px",
+                }}
+            />
+        </div>
+    );
+};
+
+export default ChannelSkeleton;
