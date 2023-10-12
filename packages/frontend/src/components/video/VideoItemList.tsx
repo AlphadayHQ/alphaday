c86d3b1947e0ce7de5d9d83402dd1ba9d4d74681
Xharles <charlesnzubechukwu000@gmail.com>
2023-10-12 18:36:45 +0100

    add videoItem List

---
+++ packages/frontend/src/components/video/VideoItemList.tsx
@@ -0,0 +1,85 @@
+import { FC, FormEvent } from "react";
+import { ItemSkeleton, ListItem, ScrollBar } from "@alphaday/ui-kit";
+import { TVideoItem } from "src/api/types";
+import { shouldFetchMoreItems } from "src/api/utils/itemUtils";
+import globalMessages from "src/globalMessages";
+
+interface IVideoItemList {
+    videos: TVideoItem[] | undefined;
+    handlePaginate: (type: "next" | "previous") => void;
+    onClick: ((id: number) => MaybeAsync<void>) | undefined;
+    onBookmark: (id: TVideoItem) => MaybeAsync<void>;
+    isAuthenticated?: boolean;
+    handlePlay: (item: TVideoItem) => MaybeAsync<void>;
+    selectedVideo: TVideoItem | null;
+}
+const VideoItemList: FC<IVideoItemList> = ({
+    videos,
+    handlePaginate,
+    onClick,
+    onBookmark,
+    isAuthenticated,
+    handlePlay,
+    selectedVideo,
+}) => {
+    const handleListScroll = ({ currentTarget }: FormEvent<HTMLElement>) => {
+        if (shouldFetchMoreItems(currentTarget)) {
+            handlePaginate("next");
+        }
+    };
+    if (!(videos === undefined)) {
+        if (videos.length === 0) {
+            return (
+                <div className="flex w-full h-full justify-center items-center bg-backgroundVariant200">
+                    <p className="text-primary fontGroup-highlight">
+                        {globalMessages.queries.noMatchFound("videos")}
+                    </p>
+                </div>
+            );
+        }
+        return (
+            <ScrollBar onScroll={handleListScroll}>
+                {videos.map((item) => {
+                    return (
+                        <ListItem
+                            key={item.id}
+                            variant="video"
+                            title={item.title}
+                            date={item.publishedAt}
+                            tag={item.sourceName}
+                            tagImg={item.sourceIcon}
+                            description={item.shortDescription}
+                            bookmarked={item.bookmarked}
+                            onClick={async () => {
+                                if (handlePlay !== undefined) {
+                                    await handlePlay(item);
+                                }
+                                if (onClick !== undefined) {
+                                    await onClick(item.id);
+                                }
+                            }}
+                            onBookmark={() => onBookmark(item)}
+                            isAuthenticated={isAuthenticated}
+                            isPlaying={item.id === selectedVideo?.id}
+                            image={item.image}
+                        />
+                    );
+                })}
+            </ScrollBar>
+        );
+    }
+    return (
+        <ScrollBar>
+            {Array.from(Array(9), Math.random).map((item) => {
+                return (
+                    <div className="abs" key={item}>
+                        <ItemSkeleton />
+                        <hr className="m-0 border-btnRingVariant500" />
+                    </div>
+                );
+            })}
+        </ScrollBar>
+    );
+};
+
+export default VideoItemList;
