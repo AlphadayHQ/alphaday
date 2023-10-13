12b7da28b94d7aa691c3cc04021c9920a728e25d
Xharles <charlesnzubechukwu000@gmail.com>
2023-10-13 14:10:45 +0100

    improve skeletons

--- packages/ui-kit/src/components/skeletons/ItemSkeleton.tsx
+++ packages/ui-kit/src/components/skeletons/ItemSkeleton.tsx
@@ -1,23 +1,30 @@
 import Skeleton from "react-loading-skeleton";
+import { twMerge } from "tailwind-merge";
+import "react-loading-skeleton/dist/skeleton.css";
+import styles from "./Skeleton.module.scss";
 
 export const ItemSkeleton = () => {
     return (
-        <div className="p-[5px] pl-[10px] flex flex-col bg-backgroundVariant200 hover:bg-backgroundVariant200">
+        <div className="py-2 pr-1.5 pl-[10px] flex flex-row bg-backgroundVariant200 hover:bg-backgroundVariant200">
             <div className="w-[50px] h-auto [&>a]:block [&:img]:w-[50px] [&:img]:h-[50px] [&:img]:rounded-[4px] [&:img]:object-cover">
                 <Skeleton
-                    style={{
-                        width: "50px",
-                        height: "50px",
-                        borderRadius: "4px",
-                    }}
+                    className={twMerge(
+                        "rounded-[4px] w-[50px] h-[50px]",
+                        styles["custom-skeleton"]
+                    )}
                 />
             </div>
-            <div className="flex-1 mt-0 ml-[10px] two-col:ml-5">
+            <div className="flex-1 mt-1 ml-[10px] two-col:ml-5">
                 <p className="uppercase fontGroup-supportBold mb-[5px] block text-primary">
-                    <Skeleton style={{ maxWidth: "70px" }} />
+                    <Skeleton
+                        className={twMerge(
+                            "max-w-[70px]",
+                            styles["custom-skeleton"]
+                        )}
+                    />
                 </p>
-                <h6 className="[&>a]:text-btnBackgroundVariant1800">
-                    <Skeleton />
+                <h6 className="[&>a]:text-btnBackgroundVariant1800 mb-0">
+                    <Skeleton className={styles["custom-skeleton"]} />
                 </h6>
             </div>
         </div>
