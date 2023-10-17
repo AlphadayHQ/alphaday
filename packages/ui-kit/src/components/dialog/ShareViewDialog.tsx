cb280d4544c33a63139b3d789a682bc05b827326
Xharles <charlesnzubechukwu000@gmail.com>
2023-10-17 22:05:01 +0100

    style improvements

--- packages/ui-kit/src/components/dialog/ShareViewDialog.tsx
+++ packages/ui-kit/src/components/dialog/ShareViewDialog.tsx
@@ -16,28 +16,34 @@ export const ShareViewDialog: FC<IProps> = ({
     show,
     title,
 }) => (
-    <Dialog title={title} onClose={onClose} showXButton showDialog={show}>
+    <Dialog
+        size="sm"
+        title={title}
+        onClose={onClose}
+        showXButton
+        showDialog={show}
+    >
         <div className="overflow-auto">
             <p>You can share your board by simply copying this URL:</p>
-            <div className="flex flex-1 flex-row justify-between font-montserrat text-primaryVariant200 bg-background rounded-[5px] p-[11px] my-5 mx-0 hover:bg-backgroundVariant200">
+            <div
+                role="button"
+                tabIndex={0}
+                onClick={onCopyUrl}
+                title="Copy board URL"
+                className="flex flex-1 flex-row justify-between font-montserrat text-primaryVariant200 bg-background rounded-[5px] p-[11px] my-5 mx-0 hover:bg-backgroundVariant500 active:bg-backgroundVariant1200 cursor-pointer"
+            >
                 <span className="flex flex-[5] overflow-hidden">
                     <span className="overflow-hidden whitespace-nowrap text-ellipsis">
                         {viewUrl}
                     </span>
                 </span>
                 <span className="flex items-center before:[content:'|'] before:my-0 before:mx-[10px] before:text-backgroundVariant200 font-700" />
-                <span
-                    className="flex flex-1 cursor-pointer items-center"
-                    role="button"
-                    tabIndex={0}
-                    onClick={onCopyUrl}
-                    title="Copy board URL"
-                >
-                    <CopySVG className="mr-[5] text-inherit" />
+                <span className="flex flex-1 cursor-pointer items-center">
+                    <CopySVG className="mr-[5px] text-inherit" />
                     Copy
                 </span>
             </div>
-            <p>
+            <p className="pb-2">
                 Users with the link will get read-only access, so they will not
                 be able to edit your board.
             </p>
