b207d6fbabbfe08cd5c5cdad6ebdea57e67c32c1
elcharitas <jonathanirhodia@gmail.com>
2023-10-18 16:09:41 +0100

    reorder definition

--- packages/ui-kit/src/components/overlay/Overlay.tsx
+++ packages/ui-kit/src/components/overlay/Overlay.tsx
@@ -1,6 +1,39 @@
 import { HTMLProps, FC } from "react";
 import { twMerge } from "tailwind-merge";
 
+interface IOverlayWrapperProps extends HTMLProps<HTMLDivElement> {
+    /**
+     * Whether the overlay is visible or not.
+     */
+    isVisible: boolean;
+}
+
+const OverlayWrapper: FC<IOverlayWrapperProps> = ({
+    isVisible,
+    className,
+    children,
+    style,
+    ...props
+}) => {
+    return (
+        <div
+            className={twMerge(
+                "block fixed overflow-hidden bg-backgroundVariant900 rounded-[5px]",
+                className
+            )}
+            style={{
+                display: isVisible ? "block" : "none",
+                ...style,
+            }}
+            {...props}
+        >
+            <div className="relative flex flex-col flex-wrap items-center justify-center overflow-hidden">
+                {children}
+            </div>
+        </div>
+    );
+};
+
 interface IOverlayProps extends IOverlayWrapperProps {
     /**
      * The position of the overlay.
@@ -60,38 +93,5 @@ export const Overlay: FC<IOverlayProps> = ({
         >
             {children}
         </OverlayWrapper>
-    );
-};
-
-interface IOverlayWrapperProps extends HTMLProps<HTMLDivElement> {
-    /**
-     * Whether the overlay is visible or not.
-     */
-    isVisible: boolean;
-}
-
-const OverlayWrapper: FC<IOverlayWrapperProps> = ({
-    isVisible,
-    className,
-    children,
-    style,
-    ...props
-}) => {
-    return (
-        <div
-            className={twMerge(
-                "block fixed overflow-hidden bg-backgroundVariant900 rounded-[5px]",
-                className
-            )}
-            style={{
-                display: isVisible ? "block" : "none",
-                ...style,
-            }}
-            {...props}
-        >
-            <div className="relative flex flex-col flex-wrap items-center justify-center overflow-hidden">
-                {children}
-            </div>
-        </div>
     );
 };
