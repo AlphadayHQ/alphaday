40788a30283490fe9118c70ae4344fde714814cb
Xharles <charlesnzubechukwu000@gmail.com>
2023-10-20 12:46:51 +0100

    document hook

--- packages/frontend/src/components/dynamic-modules/hooks/useDynamicWidgetItem.ts
+++ packages/frontend/src/components/dynamic-modules/hooks/useDynamicWidgetItem.ts
@@ -7,6 +7,20 @@ interface IDynamicWidgetHeight {
     toggleAccordion: () => void;
 }
 
+/**
+ * This hook is used in the DynamicWidget components (Agenda, FAQ, Roadmap)
+ * to update the scroll-height of the widget when one or more widget item's height changes
+ * i.e the accordion is open or closed (the description is shown or hidden).
+ *
+ * We need this because the height of the widget is adjustable, hence the height is set to
+ * a fixed value. This value must be updated when the height of the widget items changes.
+ *
+ * @param setItemsHeight - function to set the height of the widget
+ *
+ * @returns {IDynamicWidgetHeight} - object with: the height of the description, the reference to the description element,
+ * the state of the accordion and the function to toggle the accordion
+ */
+
 export const useDynamicWidgetItem: (arg: {
     setItemsHeight: React.Dispatch<React.SetStateAction<number>>;
 }) => IDynamicWidgetHeight = ({ setItemsHeight }) => {
