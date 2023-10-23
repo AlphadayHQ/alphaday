23c98af4911b485e5fbabcb3da7a5ef8f4757871
elcharitas <jonathanirhodia@gmail.com>
2023-10-23 12:37:32 +0100

    disable eslint rule

--- packages/ui-kit/src/components/table/KeyValueTable.tsx
+++ packages/ui-kit/src/components/table/KeyValueTable.tsx
@@ -13,11 +13,13 @@ export const KeyValueTable: FC<IKeyValueTable> = memo(function KeyValueTable({
         <div className="flex flex-col p-3 m-0 border-solid border-1 border-btnRingVariant300 rounded">
             {items.map((row, key) => (
                 <div
+                    // eslint-disable-next-line react/no-array-index-key
                     key={String(key)}
                     className="flex items-center justify-between mb-3 pb-3 border-b border-btnRingVariant300"
                 >
                     {row.map(({ title, value }, rKey) => (
                         <div
+                            // eslint-disable-next-line react/no-array-index-key
                             key={String(rKey)}
                             className="[&:not(:first-child)]:text-right [&:not(:first-child)]:ml-1 [&:not(:first-child)]:text-primaryVariant800 [&_.small]:text-xs [&_.small]:text-primaryVariant800"
                         >
