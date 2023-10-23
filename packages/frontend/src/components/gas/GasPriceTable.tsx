1615c559ddea1f65221cefa0346a7e836ddd2d0c
elcharitas <jonathanirhodia@gmail.com>
2023-10-23 12:16:46 +0100

    add price table

---
+++ packages/frontend/src/components/gas/GasPriceTable.tsx
@@ -0,0 +1,69 @@
+import { FC } from "react";
+import { KeyValueTable } from "@alphaday/ui-kit";
+
+const formatNumber = (value: number) =>
+    new Intl.NumberFormat("en-US", {
+        maximumFractionDigits: 2,
+    }).format(value);
+
+export type TGasPriceTable = {
+    currentSlot: number;
+    finalizedSlot: number;
+    currentEpoch: number;
+    finalizedEpoch: number;
+    activeValidators: number;
+    avgValidatorBalance: number;
+    rewardsApr: number;
+};
+
+const GasPriceTable: FC<TGasPriceTable> = ({
+    currentEpoch,
+    currentSlot,
+    activeValidators,
+    avgValidatorBalance,
+    finalizedEpoch,
+    finalizedSlot,
+}) => {
+    const items = [
+        [
+            {
+                title: "Current Slot",
+                value: formatNumber(currentSlot),
+            },
+            {
+                title: "Finalized Slot",
+                value: formatNumber(finalizedSlot),
+            },
+        ],
+        [
+            {
+                title: "Current Epoch",
+                value: formatNumber(currentEpoch),
+            },
+            {
+                title: "Finalized Epoch",
+                value: formatNumber(finalizedEpoch),
+            },
+        ],
+        [
+            {
+                title: "Active Validators",
+                value: formatNumber(activeValidators),
+            },
+            {
+                title: "Average Validator Balance",
+                value: `${formatNumber(avgValidatorBalance)} ETH`,
+            },
+        ],
+    ];
+    return (
+        <>
+            <div className="w-full text-uppercase text-primary mb-5 mt-4">
+                Beacon Chain Data
+            </div>
+            <KeyValueTable items={items} />
+        </>
+    );
+};
+
+export default GasPriceTable;
