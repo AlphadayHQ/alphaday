a579a15a16d5597d0467a8c5054835dbe9083b7f
elcharitas <jonathanirhodia@gmail.com>
2023-10-24 06:53:23 +0100

    add map title prop

--- packages/frontend/src/components/maps/MapModule.tsx
+++ packages/frontend/src/components/maps/MapModule.tsx
@@ -1,6 +1,11 @@
 import { FC, memo } from "react";
 
-const MapModule: FC<{ url: string }> = memo(function MapModule({ url }) {
-    return <iframe src={url} className="w-full h-full border-0" />;
+const MapModule: FC<{ url: string; title?: string }> = memo(function MapModule({
+    url,
+    title,
+}) {
+    return (
+        <iframe src={url} className="w-full h-full border-0" title={title} />
+    );
 });
 export default MapModule;
