1c81572ba19c32c2e76276a75c1efe0a2e2362a6
elcharitas <jonathanirhodia@gmail.com>
2023-10-26 07:53:13 +0100

    add chat form component to ui-kit

---
+++ packages/ui-kit/src/components/chat/ChatForm.tsx
@@ -0,0 +1,100 @@
+import {
+    ChangeEvent,
+    FC,
+    KeyboardEvent,
+    useCallback,
+    useMemo,
+    useRef,
+    useState,
+} from "react";
+import { ReactComponent as Send } from "src/assets/svg/send.svg";
+
+interface IForm {
+    handleSend: (m: string) => void;
+    isAuthenticated: boolean;
+}
+export const ChatForm: FC<IForm> = ({ handleSend, isAuthenticated }) => {
+    const textAreaRef = useRef<HTMLTextAreaElement | null>(null);
+    const [message, setMessage] = useState("");
+
+    const isMessageInvalid = useMemo(() => message === "\n" || message === "", [
+        message,
+    ]);
+
+    const autoGrow = useCallback(() => {
+        if (!textAreaRef.current) {
+            return;
+        }
+        if (message === "") {
+            textAreaRef.current.style.height = "auto";
+        } else {
+            textAreaRef.current.style.height = "5px";
+            textAreaRef.current.style.height = `${textAreaRef.current.scrollHeight}px`;
+        }
+    }, [message]);
+
+    const handleMessage = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setMessage(e.target.value);
+        autoGrow();
+    };
+
+    const handleSubmit = useCallback(() => {
+        if (isMessageInvalid) {
+            return;
+        }
+        handleSend(message);
+        setMessage("");
+        if (!textAreaRef.current) {
+            return;
+        }
+        textAreaRef.current.style.height = "auto";
+    }, [handleSend, isAuthenticated, isMessageInvalid, message]);
+
+    const handleKeyPress = useCallback(
+        (e: KeyboardEvent<HTMLTextAreaElement>) => {
+            if (e.key === "Enter" && message !== "") {
+                if (e.shiftKey || e.ctrlKey) {
+                    return;
+                }
+                handleSubmit();
+            }
+        },
+        [handleSubmit, message]
+    );
+
+    return (
+        <div className="h-full w-full p-4 flex justify-between bg-backgroundVariant200 border-t border-background">
+            <div>
+                <textarea
+                    ref={textAreaRef}
+                    className="font-normal border-0 bg-transparent self-center text-primary w-full resize-none overflow-hidden min-h-auto transition-none"
+                    name="message"
+                    placeholder={
+                        isAuthenticated
+                            ? "Message..."
+                            : "Connect your wallet to chat"
+                    }
+                    rows={1}
+                    onInput={autoGrow}
+                    value={message}
+                    onChange={handleMessage}
+                    onKeyPress={handleKeyPress}
+                />
+            </div>
+            <button
+                disabled={isMessageInvalid}
+                title={
+                    isAuthenticated
+                        ? "Send Message"
+                        : "Verify your wallet to chat"
+                }
+                onClick={handleSubmit}
+                className={`w-12 flex items-center justify-center bg-backgroundVariant500 border-none border-l border-background cursor-pointer ${
+                    isAuthenticated ? "" : "opacity-40 cursor-auto !important"
+                }`}
+            >
+                <Send />
+            </button>
+        </div>
+    );
+};
