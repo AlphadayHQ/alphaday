90d7ff290647bdf415a878cb658619f5f53747ae
Xharles <charlesnzubechukwu000@gmail.com>
2023-11-13 13:50:15 +0100

    use correct text style

--- packages/ui-kit/src/components/listItem/NoItems.tsx
+++ packages/ui-kit/src/components/listItem/NoItems.tsx
@@ -2,7 +2,7 @@ import { FC, ReactNode } from "react";
 
 export const NoItems: FC<{ children: ReactNode }> = ({ children }) => {
     return (
-        <div className="flex h-full w-full items-center justify-center bg-[color:var(--backgroundVariant200)]">
+        <div className="flex h-full w-full items-center justify-center bg-backgroundVariant200 prose-p:fontGroup-highlight">
             {children}
         </div>
     );
