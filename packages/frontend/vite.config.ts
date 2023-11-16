4efaeff26deaccc478ae994ca593b52a38e88cc5
elcharitas <jonathanirhodia@gmail.com>
2023-11-16 15:58:55 +0100

    add css module optimze

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -3,12 +3,19 @@ import * as path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+import { optimizeCssModules } from "vite-plugin-optimize-css-modules";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [tsconfigPaths(), react(), svgr(), ViteImageOptimizer()],
+    plugins: [
+        tsconfigPaths(),
+        react(),
+        svgr(),
+        ViteImageOptimizer(), // optimize images, svgs and gifs
+        optimizeCssModules(), // optimize css modules
+    ],
     css: {
         modules: {
             localsConvention: "camelCase",
