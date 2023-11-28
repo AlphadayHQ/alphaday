a0a69f9d744fcfb2bf9e62a954a4977d73dea38b
Xharles <charlesnzubechukwu000@gmail.com>
2023-11-28 13:02:26 +0100

    fix card styles

--- packages/frontend/src/components/custom-modules/CustomCardModule.tsx
+++ packages/frontend/src/components/custom-modules/CustomCardModule.tsx
@@ -9,15 +9,13 @@ const CustomCardModule: FC<IProps> = ({ title, value }) => {
     return (
         <div className="flex flex-col justify-center items-center border border-solid border-btnRingVariant300 !rounded-2xl p-5 m-5">
             {title && (
-                <h6 className="text-primaryVariant100 fontGroup-support text-center uppercase mb-[7px]">
+                <h6 className="text-primaryVariant100 fontGroup-support text-center uppercase mb-0.5">
                     {title}
                 </h6>
             )}
             {value && (
-                <div className="whitespace-nowrap fontGroup-major text-center">
-                    <div>
-                        <p className="text-primary mb-0 text-center">{value}</p>
-                    </div>
+                <div className="whitespace-nowrap prose-p:fontGroup-major prose-p:!text-4xl text-primary mb-0 text-center">
+                    {value}
                 </div>
             )}
         </div>
