7b62fc70ab83c1faba4fdb4adda3762a412400a0
Xharles <charlesnzubechukwu000@gmail.com>
2024-01-18 15:44:20 +0100

    Add scrollbar

--- packages/frontend/src/layout/MobileLayout.tsx
+++ packages/frontend/src/layout/MobileLayout.tsx
@@ -1,13 +1,16 @@
 import { FC, ReactNode } from "react";
-import { NavBottom, NavHeader } from "@alphaday/ui-kit";
+import { NavBottom, NavHeader, ScrollBar } from "@alphaday/ui-kit";
 
-const MobileLayout: FC<{ children: ReactNode }> = ({ children }) => {
+const MobileLayout: FC<{
+    children: ReactNode;
+    onScroll?: () => void;
+}> = ({ children, onScroll }) => {
     return (
-        <div>
+        <ScrollBar onScroll={onScroll}>
             <NavHeader avatar={undefined} />
             {children}
             <NavBottom />
-        </div>
+        </ScrollBar>
     );
 };
 
