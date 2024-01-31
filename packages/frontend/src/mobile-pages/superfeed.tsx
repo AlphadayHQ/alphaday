c9f7706c85e72f135b70e7c1f0ee365c6d43bcb2
Xharles <charlesnzubechukwu000@gmail.com>
2024-01-31 19:39:47 +0100

    add filter button

--- packages/frontend/src/mobile-pages/superfeed.tsx
+++ packages/frontend/src/mobile-pages/superfeed.tsx
@@ -1,5 +1,7 @@
+import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { ReactComponent as SettingsSVG } from "src/assets/icons/settings.svg";
+import { ReactComponent as Settings2SVG } from "src/assets/icons/settings3.svg";
 import MobileLayout from "src/layout/MobileLayout";
 import { FeedCard } from "src/mobile-components/superfeed/FeedCard";
 import {
@@ -364,17 +366,57 @@ const feedItems: IFeedItem[] = [
     },
 ];
 
-const FiltersButton = () => (
-    <Link
-        className="flex justify-between mt-2 mb-3 mx-5 px-4 py-2 border border-accentVariant100 rounded-lg"
-        to="/filters"
-    >
-        <p className="m-0 pr-2 fontGroup-highlight self-center">
-            Craft your superfeed with personalized filters
-        </p>
-        <SettingsSVG className="w-6 text-accentVariant100 mt-[3px]" />
-    </Link>
-);
+const FiltersButton = () => {
+    const showPosition = 492;
+    const element1: React.Ref<HTMLAnchorElement> = useRef(null);
+    const element2: React.Ref<HTMLAnchorElement> = useRef(null);
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (
+                window.pageYOffset > showPosition &&
+                !element1?.current?.classList.contains("scroll-show") &&
+                element1?.current
+            ) {
+                element1?.current?.classList.remove("scroll-hide");
+                element1?.current?.classList.add("scroll-show");
+                element2?.current?.classList.add("scroll-hide");
+                element2?.current?.classList.remove("scroll-show");
+            }
+
+            if (
+                window.pageYOffset < showPosition &&
+                element1?.current?.classList.contains("scroll-show") &&
+                element1?.current
+            ) {
+                element1?.current?.classList.remove("scroll-show");
+                element1?.current?.classList.add("scroll-hide");
+                element2?.current?.classList.add("scroll-show");
+                element2?.current?.classList.remove("scroll-hide");
+            }
+        });
+    }, []);
+    return (
+        <>
+            <Link
+                ref={element1}
+                className="flex justify-between mt-2 mb-3 mx-5 px-4 py-2 border border-accentVariant100 rounded-lg"
+                to="/filters"
+            >
+                <p className="m-0 pr-2 fontGroup-highlight self-center">
+                    Craft your superfeed with personalized filters
+                </p>
+                <SettingsSVG className="w-6 text-accentVariant100 mt-[3px]" />
+            </Link>
+            <Link
+                ref={element2}
+                to="/filters"
+                className="absolute bg-accentVariant100 rounded-lg p-4 bottom-24 right-5"
+            >
+                <Settings2SVG className="w-6 text-primary" />
+            </Link>
+        </>
+    );
+};
 
 const SuperfeedPage = () => {
     return (
