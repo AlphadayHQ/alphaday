35d1da0b03881b12eea64dcfe1d7923867f7f3d6
Xharles <charlesnzubechukwu000@gmail.com>
2024-02-09 21:33:32 +0100

    clean up

--- packages/frontend/src/mobile-pages/filters.tsx
+++ packages/frontend/src/mobile-pages/filters.tsx
@@ -2,7 +2,8 @@ import MobileLayout from "src/layout/MobileLayout";
 
 const FiltersPage = () => {
     return (
-        <MobileLayout>
+        // Work around. this file is deleted in another PR
+        <MobileLayout toggleShowUserMenu={() => {}}>
             <h1 className="mx-6 my-10 fontGroup-major">Filters Page</h1>
         </MobileLayout>
     );
