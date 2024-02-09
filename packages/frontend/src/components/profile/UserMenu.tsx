c61f5cbd35d96b7437b1e81501dd64d4e4a8b9f6
Xharles <charlesnzubechukwu000@gmail.com>
2024-02-09 20:55:34 +0100

    create user menu

---
+++ packages/frontend/src/components/profile/UserMenu.tsx
@@ -0,0 +1,118 @@
+import { FC } from "react";
+import { FullPageModal, twMerge } from "@alphaday/ui-kit";
+import { Link, useHistory } from "react-router-dom";
+import { ReactComponent as ChevronSVG } from "src/assets/icons/chevron-down2.svg";
+import { ReactComponent as DocSVG } from "src/assets/icons/doc.svg";
+import { ReactComponent as LogoutSVG } from "src/assets/icons/logout.svg";
+import { ReactComponent as StarSVG } from "src/assets/icons/star.svg";
+import { ReactComponent as UserSVG } from "src/assets/icons/user.svg";
+
+const menu = [
+    {
+        id: 1,
+        icon: UserSVG,
+        title: "Edit profile",
+        subtext: "Edit your profile",
+        link: "/profile",
+    },
+    {
+        id: 2,
+        icon: DocSVG,
+        title: "Privacy policy",
+        subtext: "How we work & use your data",
+        link: "profile/privacy",
+    },
+    {
+        id: 3,
+        icon: StarSVG,
+        title: "Rate us",
+        subtext: "Tell us what we think",
+        link: "profile/rate",
+    },
+    {
+        id: 4,
+        icon: LogoutSVG,
+        title: "Log out",
+        subtext: null,
+        link: "profile/log-out",
+    },
+];
+
+interface IUserMenu {
+    // avatar: string;
+    isOpen: boolean;
+    onClose: () => void;
+    isAuthenticated: boolean;
+}
+const UserMenu: FC<IUserMenu> = ({ isOpen, onClose, isAuthenticated }) => {
+    const history = useHistory();
+
+    const navigate = (link: string) => {
+        history.push(link);
+    };
+    return (
+        <FullPageModal isOpen={isOpen} closeModal={onClose}>
+            <div className="flex flex-start w-full items-center mb-4">
+                <ChevronSVG
+                    onClick={onClose}
+                    tabIndex={0}
+                    role="button"
+                    className="w-6 h-6 mr-2 rotate-180 self-center -ml-1.5"
+                />
+            </div>
+            <div className="flex flex-col flex-start w-full items-start mb-4">
+                <p className="mb-0 fontGroup-highlight">
+                    Sign up to unlock the complete experience{" "}
+                </p>
+                <button
+                    type="button"
+                    className="flex fontGroup-highlight !font-semibold py-3 px-4 bg-accentVariant100 hover:bg-accentVariant200 w-full mt-5 justify-center rounded-lg"
+                >
+                    Sign up
+                </button>
+                <p className="mt-6">
+                    <span className="mt-6">Already have an account?</span>
+                    <Link
+                        to="/#"
+                        className="ml-2 font-semibold border-b border-accentVariant100"
+                    >
+                        Log in here
+                    </Link>
+                </p>
+            </div>
+            <div className="mt-10 w-full">
+                {menu.map((item) => (
+                    <div
+                        onClick={() =>
+                            isAuthenticated ? navigate(item.link) : {}
+                        }
+                        tabIndex={0}
+                        role="button"
+                        key={item.id}
+                        className={twMerge(
+                            "flex w-full justify-start items-center border-b border-primaryVariant100 pb-2.5 pt-2.5 last:pb-0 last:border-none first:pt-0 cursor-pointer",
+                            isAuthenticated
+                                ? "text-primary"
+                                : "text-primaryVariant100"
+                        )}
+                    >
+                        <item.icon className="mr-4 w-6 h-6" />
+                        <div className="flex flex-grow min-w-max justify-between items-center">
+                            <div className="flex flex-col w-full">
+                                <span className="block fontGroup-highlightSemi">
+                                    {item.title}
+                                </span>
+                                <span className="fontGroup-support">
+                                    {item.subtext}
+                                </span>
+                            </div>
+                            <ChevronSVG className="h-3.5" />
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </FullPageModal>
+    );
+};
+
+export default UserMenu;
