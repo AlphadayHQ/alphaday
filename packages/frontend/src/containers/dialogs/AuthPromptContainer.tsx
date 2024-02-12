718ad98821c0ddaf8ed6ebd2afa8a8459ce183e9
elcharitas <jonathanirhodia@gmail.com>
2024-02-12 08:29:15 +0100

    add timne-based visibility check for prompt

--- packages/frontend/src/containers/dialogs/AuthPromptContainer.tsx
+++ packages/frontend/src/containers/dialogs/AuthPromptContainer.tsx
@@ -1,15 +1,35 @@
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { Modal } from "@alphaday/ui-kit";
 import { useAuth } from "src/api/hooks";
+import { useAppDispatch, useAppSelector } from "src/api/store/hooks";
+import * as uiStore from "src/api/store/slices/ui";
 import { EAuthMethod } from "src/api/types";
 import { AuthMethodSelection } from "src/components/auth/AuthModule";
 
 const AuthPromptContainer = () => {
-    // TODO:add logic to store and check if the auth prompt should be visible within 7days/14days
-    const [isAuthPromptVisible, setIsPromptVisible] = useState(true);
-    const { ssoLogin } = useAuth();
-    const [isSignIn, setIsSignIn] = useState(false);
+    const dispatch = useAppDispatch();
+    const lastAuthPromptedTs = useAppSelector(
+        (state) => state.ui.lastAuthPrompted
+    );
+    const setLastAuthPrompted = useCallback(() => {
+        dispatch(uiStore.setLastAuthPrompted(new Date().getTime()));
+    }, [dispatch]);
 
+    const { ssoLogin, isAuthenticated } = useAuth();
+    const isAuthPromptVisible = useMemo(() => {
+        if (!lastAuthPromptedTs) {
+            setLastAuthPrompted();
+            return true;
+        }
+        // check if the auth prompt should be visible within 7days/14days
+        const now = new Date().getTime();
+        return (
+            !isAuthenticated &&
+            now - lastAuthPromptedTs > 7 * 24 * 60 * 60 * 1000
+        ); // 7days
+    }, [lastAuthPromptedTs, setLastAuthPrompted, isAuthenticated]);
+
+    const [isSignIn, setIsSignIn] = useState(false);
     const handleSSOCallback = useCallback(
         (method: EAuthMethod) => {
             ssoLogin(method);
@@ -22,7 +42,7 @@ const AuthPromptContainer = () => {
         <Modal
             showModal={isAuthPromptVisible}
             className="fixed bottom-[60px] m-0 w-full p-8"
-            onClose={() => setIsPromptVisible(false)}
+            onClose={setLastAuthPrompted}
         >
             <h2 className="text-primary text-center text-2xl font-semibold leading-loose">
                 {isSignIn ? "Sign in to" : "Sign up for"} Alphaday
