ba970fe25ba28e98ee18368255aa80cd43a13306
elcharitas <jonathanirhodia@gmail.com>
2024-02-12 08:35:24 +0100

    cleanup navheader changes

--- packages/frontend/src/layout/MobileLayout.tsx
+++ packages/frontend/src/layout/MobileLayout.tsx
@@ -1,25 +1,13 @@
 import { FC, ReactNode } from "react";
 import { NavBottom, NavHeader } from "@alphaday/ui-kit";
-import { useHistory } from "react-router";
-import { useAuth } from "src/api/hooks";
 
 const MobileLayout: FC<{
     children: ReactNode;
     onScroll?: () => void;
 }> = ({ children, onScroll }) => {
-    const { isAuthenticated } = useAuth();
-    const history = useHistory();
     return (
         <div className="h-screen" onScroll={onScroll}>
-            <NavHeader
-                avatar={undefined}
-                onAvatarClick={() => {
-                    if (!isAuthenticated) {
-                        // TODO: Open page with login info
-                        history.push("/auth");
-                    }
-                }}
-            />
+            <NavHeader avatar={undefined} />
             {children}
             <NavBottom />
         </div>
