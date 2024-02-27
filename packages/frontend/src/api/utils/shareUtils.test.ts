707ff6c22a177c4e45a6f640e9370d1e317092c7
elcharitas <jonathanirhodia@gmail.com>
2024-02-27 07:42:10 +0100

    add test cases

---
+++ packages/frontend/src/api/utils/shareUtils.test.ts
@@ -0,0 +1,31 @@
+import { canShare, shareData } from "./shareUtils";
+
+describe("shareUtils", () => {
+    describe("canShare", () => {
+        it("should return true if the Web Share API is supported", () => {
+            expect(canShare()).toBe(true);
+        });
+
+        it("should return false if the Web Share API is not supported", () => {
+            // @ts-expect-error navigator is read-only, we're just testing
+            navigator.share = undefined;
+            expect(canShare()).toBe(false);
+        });
+    });
+
+    describe("shareData", () => {
+        it("should share the provided data using the Web Share API", () => {
+            expect(shareData({ title: "Test", text: "Test" })).resolves.toBe(
+                undefined
+            );
+        });
+
+        it("should not share the data if the Web Share API is not supported", () => {
+            // @ts-expect-error navigator is read-only, we're just testing
+            navigator.share = undefined;
+            expect(shareData({ title: "Test", text: "Test" })).rejects.toThrow(
+                "Sharing not supported"
+            );
+        });
+    });
+});
