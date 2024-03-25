f66b67be8d3b4fbcdb225bf0bda8055943640126
Xharles <charlesnzubechukwu000@gmail.com>
2024-03-25 16:07:31 +0100

    move buttons out of collapse trigger - image card

--- packages/frontend/src/mobile-components/feed/ImageCard.tsx
+++ packages/frontend/src/mobile-components/feed/ImageCard.tsx
@@ -68,30 +68,6 @@ export const ImageCard: FC<{
                                     </div>
                                 </div>
                             </div>
-                            <div className="flex justify-between">
-                                <div className="flex-col">
-                                    {!open && (
-                                        <TagButtons
-                                            truncated
-                                            tags={tags}
-                                            onClick={() => {}}
-                                        />
-                                    )}
-                                </div>
-                                <div className="flex-col min-w-max ml-2">
-                                    {!open && (
-                                        <ActionButtons
-                                            onLike={onLike}
-                                            onCommentClick={onLike}
-                                            onShare={onShare}
-                                            likes={likes}
-                                            comments={comments}
-                                            isAuthenticated={isAuthenticated}
-                                            isLiked={item.isLiked}
-                                        />
-                                    )}
-                                </div>
-                            </div>
                         </div>
                     </FeedItemDisclosureButton>
                     <FeedItemDisclosurePanel>
@@ -120,6 +96,26 @@ export const ImageCard: FC<{
                             </div>
                         </div>
                     </FeedItemDisclosurePanel>
+                    <div className="flex justify-between my-2">
+                        <div className="flex-col">
+                            <TagButtons
+                                truncated
+                                tags={tags}
+                                onClick={() => {}}
+                            />
+                        </div>
+                        <div className="flex-col min-w-max ml-2">
+                            <ActionButtons
+                                isAuthenticated={isAuthenticated}
+                                onLike={onLike}
+                                onCommentClick={onLike}
+                                onShare={onShare}
+                                likes={likes}
+                                comments={comments}
+                                isLiked={item.isLiked}
+                            />
+                        </div>
+                    </div>
                 </>
             )}
         </FeedItemDisclosure>
