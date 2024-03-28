8f0c9da93941277843d8b1151b6e489ae3760700
Jonathan Irhodia <jonathanirhodia@gmail.com>
2024-03-28 11:56:02 +0100

    chore: set up new likes endpoint (#371)

--- packages/frontend/src/mobile-containers/SuperfeedContainer.tsx
+++ packages/frontend/src/mobile-containers/SuperfeedContainer.tsx
@@ -208,7 +208,8 @@ const SuperfeedContainer: FC<{
         async (item: TSuperfeedItem) => {
             try {
                 await likeSuperfeedItemMut({
-                    id: item.id,
+                    itemId: item.itemId,
+                    contentType: item.type,
                 }).unwrap();
             } catch (e) {
                 Logger.error("SuperfeedModule::FeedCard: error liking item", e);
