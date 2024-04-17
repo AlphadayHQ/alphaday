ba704318ac64276ce40cfbf938f6b97f9f171163
elcharitas <jonathanirhodia@gmail.com>
2024-04-17 15:11:15 +0100

    add support for history

--- packages/frontend/src/api/store/providers/controlled-modal-provider.tsx
+++ packages/frontend/src/api/store/providers/controlled-modal-provider.tsx
@@ -1,17 +1,22 @@
-import { createContext, useContext, useMemo, useState } from "react";
+import {
+    createContext,
+    useCallback,
+    useContext,
+    useMemo,
+    useState,
+} from "react";
 
-interface ControlledModalContextProps {
+interface Prop {
     activeModal: string | null;
-    setActiveModal: React.Dispatch<React.SetStateAction<string | null>>;
+    setActiveModal: (modalId: string) => void;
     closeModal: () => void;
 }
 
-export const ControlledModalContext =
-    createContext<ControlledModalContextProps>({
-        activeModal: null,
-        setActiveModal: () => {},
-        closeModal: () => {},
-    });
+export const ControlledModalContext = createContext<Prop>({
+    activeModal: null,
+    setActiveModal: () => {},
+    closeModal: () => {},
+});
 
 export const useControlledModal = () => {
     const context = useContext(ControlledModalContext);
@@ -23,17 +28,38 @@ export const useControlledModal = () => {
     return context;
 };
 
+const MODAL_HISTORY = new Set<string>();
+
 const ControlledModalProvider: React.FC<{ children?: React.ReactNode }> = ({
     children,
 }) => {
     const [activeModal, setActiveModal] = useState<string | null>(null);
-    const closeModal = () => setActiveModal(null);
+
+    const closeModal = useCallback(() => {
+        if (activeModal) {
+            MODAL_HISTORY.delete(activeModal);
+            // set active modal to last modal in history
+            const lastModal = Array.from(MODAL_HISTORY).pop();
+            setActiveModal(lastModal || null);
+        }
+    }, [activeModal]);
+
+    const setActiveModalWithHistory = (modalId: string) => {
+        if (!MODAL_HISTORY.has(modalId)) {
+            setActiveModal(modalId);
+            MODAL_HISTORY.add(modalId);
+        }
+    };
 
     return (
         <ControlledModalContext.Provider
             value={useMemo(
-                () => ({ activeModal, setActiveModal, closeModal }),
-                [activeModal]
+                () => ({
+                    activeModal,
+                    setActiveModal: setActiveModalWithHistory,
+                    closeModal,
+                }),
+                [activeModal, closeModal]
             )}
         >
             {children}
