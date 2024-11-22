b9f834824af791a48b8d38c5808c6d7c6e23917a
Xharles <charlesnzubechukwu000@gmail.com>
2024-11-22 14:27:35 +0100

    update date

--- packages/frontend/src/api/utils/dateUtils.ts
+++ packages/frontend/src/api/utils/dateUtils.ts
@@ -1,3 +1,4 @@
+import i18next from "i18next";
 import moment from "moment";
 /**
  *  returns a text that represents the given datetime
@@ -8,19 +9,19 @@ import moment from "moment";
 export const computeDuration = (date: string | Date): string => {
     moment.updateLocale("en", {
         relativeTime: {
-            future: "in %s", // this shouldn't occur If it does there's an error.
-            past: "%s ago",
-            s: "%ds",
-            m: "%dm",
-            mm: "%dm",
-            h: "%dh",
-            hh: "%dh",
-            d: "%dd",
-            dd: "%dd",
-            M: "%dmo",
-            MM: "%dmo",
-            y: "%dy",
-            yy: "%dy",
+            future: i18next.t("datelocale.future"), // this shouldn't occur If it does there's an error.
+            past: i18next.t("datelocale.past"),
+            s: i18next.t("datelocale.s"),
+            m: i18next.t("datelocale.m"),
+            mm: i18next.t("datelocale.mm"),
+            h: i18next.t("datelocale.h"),
+            hh: i18next.t("datelocale.hh"),
+            d: i18next.t("datelocale.d"),
+            dd: i18next.t("datelocale.dd"),
+            M: i18next.t("datelocale.M"),
+            MM: i18next.t("datelocale.MM"),
+            y: i18next.t("datelocale.y"),
+            yy: i18next.t("datelocale.yy"),
         },
     });
 
