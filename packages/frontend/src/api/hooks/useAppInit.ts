206d74c9fe114e6f056ea56538994fd600446597
Jonathan Irhodia <jonathanirhodia@gmail.com>
2024-12-05 09:05:14 +0100

    fix: random auth related blank screens (#477)

--- packages/frontend/src/api/hooks/useAppInit.ts
+++ packages/frontend/src/api/hooks/useAppInit.ts
@@ -7,12 +7,27 @@ import {
     useGetFeaturesQuery,
     useGetSubscribedViewsQuery,
 } from "src/api/services";
+import * as userStore from "src/api/store/slices/user";
+import { useAppDispatch } from "../store/hooks";
+import { getRtkErrorCode } from "../utils/errorHandling";
+import { Logger } from "../utils/logging";
 
 export const useAppInit: () => void = () => {
     /**
      * Fetch the features on app load, then we rely on redux cache to
      * ensure subsequent calls are cached.
      */
-    useGetFeaturesQuery();
+    const dispatch = useAppDispatch();
+    const { error } = useGetFeaturesQuery();
     useGetSubscribedViewsQuery();
+
+    if (getRtkErrorCode(error) === 401) {
+        /**
+         * If the status check or the subscribed views endpoint gives a 401 unauthorized error,
+         * we need to reset the auth state and reload the app
+         */
+        Logger.debug("App::AppRoutes: 401 received");
+        dispatch(userStore.resetAuthState());
+        location.reload();
+    }
 };
