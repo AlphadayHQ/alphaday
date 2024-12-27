84d5899f1a2c0df2f46295b53011d201bad4cae8
Xharles <charlesnzubechukwu000@gmail.com>
2024-12-27 07:24:57 +0100

    reload on lang change

--- packages/frontend/src/api/hooks/usePreferredLanguage.ts
+++ packages/frontend/src/api/hooks/usePreferredLanguage.ts
@@ -1,16 +1,24 @@
+import { useRef } from "react";
 import i18next from "i18next";
 import moment from "moment-with-locales-es6";
 import { useAppSelector } from "../store/hooks";
 import { Logger } from "../utils/logging";
 
 export const usePreferredLanguage = () => {
-    const selectedCode = useAppSelector(
+    const selectedLangCode = useAppSelector(
         (state) => state.ui.selectedLanguageCode
     );
-    if (selectedCode) {
-        i18next.changeLanguage(selectedCode).catch((e) => {
+    const prevLangCodeRef = useRef(selectedLangCode);
+
+    if (selectedLangCode) {
+        i18next.changeLanguage(selectedLangCode).catch((e) => {
             Logger.error("usePreferredLanguage::Error changing language::", e);
         });
-        moment.locale(selectedCode);
+        moment.locale(selectedLangCode);
+
+        if (selectedLangCode !== prevLangCodeRef.current) {
+            prevLangCodeRef.current = selectedLangCode;
+            location.reload();
+        }
     }
 };
