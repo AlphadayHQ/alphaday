fbbf1cc41123cbc4f0a6f007d282986626bbca6a
Xharles <charlesnzubechukwu000@gmail.com>
2025-01-13 15:10:07 +0100

    refetch queries on lang change

--- packages/frontend/src/api/hooks/usePreferredLanguage.ts
+++ packages/frontend/src/api/hooks/usePreferredLanguage.ts
@@ -1,24 +1,37 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import i18next from "i18next";
 import moment from "moment-with-locales-es6";
+import { useDispatch } from "react-redux";
+import { alphadayApi } from "../services";
 import { useAppSelector } from "../store/hooks";
 import { Logger } from "../utils/logging";
 
 export const usePreferredLanguage = () => {
+    const dispatch = useDispatch();
     const selectedLangCode = useAppSelector(
         (state) => state.ui.selectedLanguageCode
     );
     const prevLangCodeRef = useRef(selectedLangCode);
 
-    if (selectedLangCode) {
-        i18next.changeLanguage(selectedLangCode).catch((e) => {
-            Logger.error("usePreferredLanguage::Error changing language::", e);
-        });
-        moment.locale(selectedLangCode);
-
-        if (selectedLangCode !== prevLangCodeRef.current) {
-            prevLangCodeRef.current = selectedLangCode;
-            location.reload();
+    useEffect(() => {
+        if (!selectedLangCode || i18next.language === selectedLangCode) {
+            return;
         }
-    }
+        moment.locale(selectedLangCode);
+        i18next
+            .changeLanguage(selectedLangCode)
+            .then(() => {
+                if (selectedLangCode !== prevLangCodeRef.current) {
+                    prevLangCodeRef.current = selectedLangCode;
+                    // Reset all queries
+                    dispatch(alphadayApi.util.resetApiState());
+                }
+            })
+            .catch((e) => {
+                Logger.error(
+                    "usePreferredLanguage::Error changing language::",
+                    e
+                );
+            });
+    }, [selectedLangCode, dispatch]);
 };
