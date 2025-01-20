239b18d7b6562143755507a3b58c1c1823c70dac
Xharles <charlesnzubechukwu000@gmail.com>
2025-01-20 10:48:49 +0100

    use string literal

--- packages/frontend/src/api/types/market.ts
+++ packages/frontend/src/api/types/market.ts
@@ -38,13 +38,13 @@ export type TCoinMarketHistory = {
 };
 
 export const CHART_RANGE_OPTIONS = {
-    oneDay: { value: "1D", translationKey: `datelocale.d`, prefix: "1" },
-    oneWeek: { value: "1W", translationKey: `datelocale.w`, prefix: "1" },
-    oneMonth: { value: "1M", translationKey: `datelocale.M`, prefix: "1" },
-    threeMonths: { value: "3M", translationKey: `datelocale.M`, prefix: "3" },
+    oneDay: { value: "1D", translationKey: "datelocale.d", prefix: "1" },
+    oneWeek: { value: "1W", translationKey: "datelocale.w", prefix: "1" },
+    oneMonth: { value: "1M", translationKey: "datelocale.M", prefix: "1" },
+    threeMonths: { value: "3M", translationKey: "datelocale.M", prefix: "3" },
     yearToDate: { value: "YTD", translationKey: "datelocale.ytd", prefix: "" },
-    oneYear: { value: "1Y", translationKey: `datelocale.y`, prefix: "1" },
-    threeYear: { value: "3Y", translationKey: `datelocale.y`, prefix: "3" },
+    oneYear: { value: "1Y", translationKey: "datelocale.y", prefix: "1" },
+    threeYear: { value: "3Y", translationKey: "datelocale.y", prefix: "3" },
     all: { value: "ALL", translationKey: "navigation.general.all", prefix: "" },
 } as const;
 type TRangeKeys = keyof typeof CHART_RANGE_OPTIONS;
