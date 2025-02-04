063d658e7cceb67715a63876da07ebd815e12387
Xharles <charlesnzubechukwu000@gmail.com>
2025-02-04 16:15:18 +0100

    update slugs

--- packages/frontend/src/constants.ts
+++ packages/frontend/src/constants.ts
@@ -33,10 +33,10 @@ export enum EFeaturesRegistry {
     WalletBoard = "wallet_board",
     Notifications = "notification",
     MobileApp = "mobile_app",
-    TranslationFr = "translationFr",
-    TranslationEs = "translationFr",
-    TranslationTr = "translationFr",
-    TranslationJa = "translationFr",
+    TranslationFr = "translation_fr",
+    TranslationEs = "translation_es",
+    TranslationTr = "translation_tr",
+    TranslationJa = "translation_ja",
 }
 
 /**
