e8c0b45a1187104f6064d504a1452093c194e68a
elcharitas <jonathanirhodia@gmail.com>
2025-03-12 11:09:02 +0100

    cleanup

--- packages/frontend/src/api/utils/twitterUtils.ts
+++ packages/frontend/src/api/utils/twitterUtils.ts
@@ -1,4 +1,4 @@
-import {
+import type {
     TRemoteTweet,
     TRemoteTweetEntities,
     TRemoteTweetMedia,
@@ -6,7 +6,7 @@ import {
     TRemoteTweetUrl,
     TRemoteTweetUser,
 } from "../services";
-import {
+import type {
     TTweets,
     TTweetAttachment,
     TTweetAuthor,
@@ -79,7 +79,7 @@ export const parseRemoteMetrics = ({
     reply_count,
     like_count,
     quote_count,
-}: TRemoteTweetMetrics = {}): TTweetMetric => ({
+}: TRemoteTweetMetrics): TTweetMetric => ({
     retweets: retweet_count,
     replies: reply_count,
     likes: like_count,
@@ -107,7 +107,6 @@ export const parseAsTweet = (remoteTweet: TRemoteTweet): TTweets => {
             remoteTweet?.attachments?.attachments.map(parseRemoteMedia),
         referencedTweets: remoteTweet.referenced_tweets,
         retweet: referencedTweets?.[0].tweet,
-        metrics: parseRemoteMetrics(remoteTweet.public_metrics),
         createdAt: remoteTweet.created_at,
         lang: remoteTweet.lang,
     };
