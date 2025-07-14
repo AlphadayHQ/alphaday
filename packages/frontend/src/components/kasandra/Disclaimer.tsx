66938ac909ae290939893b7dba18a059bfb37d0b
Xharles <charlesnzubechukwu000@gmail.com>
2025-07-14 18:09:21 +0100

    increase padding

--- packages/frontend/src/components/kasandra/Disclaimer.tsx
+++ packages/frontend/src/components/kasandra/Disclaimer.tsx
@@ -9,7 +9,7 @@ const Disclaimer: FC<IDisclaimer> = ({ accepted, onAccept }) => {
     if (accepted) return null;
 
     return (
-        <div className="flex justify-between items-center gap-2 mt-0 bg-backgroundBlue px-2 py-2">
+        <div className="flex justify-between items-center gap-2 mt-0 bg-backgroundBlue px-3 py-2">
             <p className="fontGroup-normal text-primary mb-0">
                 These are AI-generated predictions.
             </p>
