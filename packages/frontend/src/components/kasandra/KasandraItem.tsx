010c73a4a5c390cc31288a52cc62e636c5458743
Charles Nwankwo <39612820+Xavier-Charles@users.noreply.github.com>
2025-07-24 16:54:11 +0100

    fix: Add Markdown to Kasandra Insight Rationale (#608)

--- packages/frontend/src/components/kasandra/KasandraItem.tsx
+++ packages/frontend/src/components/kasandra/KasandraItem.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-nested-ternary */
 import { FC, useMemo } from "react";
 import { twMerge } from "@alphaday/ui-kit";
+import ReactMarkdown from "react-markdown";
 import { TCoin } from "src/api/types";
 import { TInsightItem } from "src/api/types/kasandra";
 import { ENumberStyle, formatNumber } from "src/api/utils/format";
@@ -179,8 +180,8 @@ const KasandraItem: FC<{
                 >
                     {openAccordion && (
                         <div className="info ml-5 px-2 pr-3 pt-2 rounded-sm min-h-[45px] [align-self:normal]">
-                            <div className="wrap flex flex-wrap text-primary whitespace-pre-wrap">
-                                {item.rationale}
+                            <div className="wrap flex flex-wrap text-primary whitespace-pre-wrap [&>p]:mb-2 [&>p]:w-full [&>p]:[display:ruby]">
+                                <ReactMarkdown>{item.rationale}</ReactMarkdown>
                             </div>
                             <br />
                             {/* {itemSources && (
