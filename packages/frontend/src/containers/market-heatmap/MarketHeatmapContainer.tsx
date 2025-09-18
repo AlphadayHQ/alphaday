1d29252fc2126525d7cabc99e88722f8c262e45c
elcharitas <jonathanirhodia@gmail.com>
2025-09-18 07:38:24 +0100

    disable kasandra_supported flag in coins query

--- packages/frontend/src/containers/market-heatmap/MarketHeatmapContainer.tsx
+++ packages/frontend/src/containers/market-heatmap/MarketHeatmapContainer.tsx
@@ -21,7 +21,7 @@ const MarketHeatmapContainer: FC<IModuleContainer> = ({ moduleData }) => {
         isError,
     } = useGetCoinsQuery({
         limit: maxItems,
-        kasandra_supported: true,
+        kasandra_supported: false,
     });
 
     const {
