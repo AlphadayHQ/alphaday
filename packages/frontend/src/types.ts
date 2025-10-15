47d4e86b3f2849f229a6ad9f8e6f7b4a934e6960
Xharles <charlesnzubechukwu000@gmail.com>
2025-10-15 20:48:55 +0100

    register container

--- packages/frontend/src/types.ts
+++ packages/frontend/src/types.ts
@@ -122,6 +122,9 @@ export const TEMPLATES_DICT: Partial<TTemplatesDict> = {
     polymarket_template: lazyRetry(
         () => import("./containers/polymarket/PolymarketContainer")
     ),
+    polymarket_top_volume_template: lazyRetry(
+        () => import("./containers/polymarket/PolymarketTopVolumeContainer")
+    ),
     portfolio_template: lazyRetry(
         () => import("./containers/portfolio/PortfolioContainer")
     ),
