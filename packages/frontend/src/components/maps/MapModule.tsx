81556a683b00425969db69df47da82e42f745f8e
Xharles <charlesnzubechukwu000@gmail.com>
2025-12-26 14:19:52 +0100

    feat: Add height prop to MapModule and update MapContainer for mobile layout handling

--- packages/frontend/src/components/maps/MapModule.tsx
+++ packages/frontend/src/components/maps/MapModule.tsx
@@ -1,11 +1,17 @@
 import { FC, memo } from "react";
 
-const MapModule: FC<{ url: string; title?: string }> = memo(function MapModule({
-    url,
-    title,
-}) {
-    return (
-        <iframe src={url} className="w-full h-[500px] border-0" title={title} />
-    );
-});
+const MapModule: FC<{ url: string; title?: string; height?: number }> = memo(
+    function MapModule({ url, title, height }) {
+        return (
+            <iframe
+                src={url}
+                className="w-full h-[500px] border-0"
+                title={title}
+                style={{
+                    height: `${height ?? 500}px`,
+                }}
+            />
+        );
+    }
+);
 export default MapModule;
