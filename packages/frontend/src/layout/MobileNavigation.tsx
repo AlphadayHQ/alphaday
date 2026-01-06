fa4ec1580caf07d312c5f7d6fa6e98cac56345c8
Xharles <charlesnzubechukwu000@gmail.com>
2026-01-06 09:08:50 +0100

    feat: Update MobileNavigation to use icon components and add Boards tab

--- packages/frontend/src/layout/MobileNavigation.tsx
+++ packages/frontend/src/layout/MobileNavigation.tsx
@@ -3,20 +3,18 @@ import { Link, useLocation } from "react-router-dom";
 import { useIsMobile } from "src/api/hooks";
 import { ReactComponent as WidgetsSVG } from "src/assets/icons/grid.svg";
 import { ReactComponent as SuperfeedSVG } from "src/assets/svg/superfeed.svg";
+import { ReactComponent as ViewsSVG } from "src/assets/svg/views.svg";
 
 const CustomNavTab: React.FC<{
     label: string;
-    Icon: React.FC<React.SVGProps<SVGSVGElement>>;
+    icon: React.ReactNode;
     disabled?: boolean;
-}> = ({ label, Icon, disabled }) => (
+}> = ({ label, icon, disabled }) => (
     <div className="inline-flex flex-col items-center justify-center px-2">
-        <span className="rounded-2xl relative">
-            <Icon className="h-5" />
-        </span>
+        <span className="rounded-2xl relative">{icon}</span>
         <span className="mt-0.5 fontGroup-supportBold">
             {disabled ? `${label} (soon)` : label}
         </span>
-        {/* {disabled && <div className="text-xs leading-3">(soon)</div>} */}
     </div>
 );
 
@@ -37,7 +35,10 @@ const MobileNavigation = () => {
                     pathname === "/" ? "text-primary" : "text-primaryVariant100"
                 }`}
             >
-                <CustomNavTab label="Feed" Icon={SuperfeedSVG} />
+                <CustomNavTab
+                    label="Feed"
+                    icon={<SuperfeedSVG className="h-[18px]" />}
+                />
             </Link>
             <Link
                 to="/widgets"
@@ -47,7 +48,23 @@ const MobileNavigation = () => {
                         : "text-primaryVariant100"
                 }`}
             >
-                <CustomNavTab label="Widgets" Icon={WidgetsSVG} />
+                <CustomNavTab
+                    label="Widgets"
+                    icon={<WidgetsSVG className="h-[22px] -mb-0.5" />}
+                />
+            </Link>
+            <Link
+                to="/widgets"
+                className={`flex-1 flex justify-center ${
+                    pathname === "/boards"
+                        ? "text-primary"
+                        : "text-primaryVariant100"
+                }`}
+            >
+                <CustomNavTab
+                    label="Boards"
+                    icon={<ViewsSVG className="h-4" />}
+                />
             </Link>
         </nav>
     );
