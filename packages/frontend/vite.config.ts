ee0b61b7d6c2df9e3d6d7bdf64e6ae2e40aa3918
Xharles <charlesnzubechukwu000@gmail.com>
2026-05-20 09:10:46 +0100

    chore: upgrade apexcharts and shim moment for vite

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -79,6 +79,17 @@ export default defineConfig(({ mode }) => {
     }
     return {
         plugins,
+        resolve: {
+            alias: [
+                {
+                    find: /^moment-with-locales-es6$/,
+                    replacement: path.resolve(
+                        __dirname,
+                        "src/shims/moment-with-locales-es6.ts"
+                    ),
+                },
+            ],
+        },
         css: {
             modules: {
                 localsConvention: "camelCase",
